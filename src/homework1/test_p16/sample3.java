672785043fe7de65f9c29a82f6cec3c333fbf738
Tung-Yun <cathytung880606@gmail.com>
2019-10-03 14:03:04 +0800

    java1

---
+++ src/homework1/test_p16/sample3.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class sample3 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("A");
+		System.out.println("Åwªï¨Ï¥ÎJava§a!");
+		System.out.println("123");
+	}
+}
